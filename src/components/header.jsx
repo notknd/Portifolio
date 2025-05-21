ed894d4219af554f97267423f6cbffb29b19e452
notknd <kendyhashimoto@hotmail.com>
2025-05-21 12:47:34 -0300

    react integration

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header>
+            <a href="#" class="logo">Kendy</a>
+
+            <nav>
+                <a href="#" class="active">Home</a>
+                <a href="#">Services</a>
+                <a href="#">Skills</a>
+                <a href="#">Education</a>
+                <a href="#">Experiences</a>
+                <a href="#">Contact</a>
+            </nav>
+
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/main.jsx
@@ -0,0 +1,29 @@
+const Main = () => {
+    return (
+        <section class="home">
+            <div class="home-img">
+                <img src="./img/Perfil.png" alt="" />
+            </div>
+            <div class="home_content">
+                <h1>Hi, i'm <span>Kendy</span></h1>
+                <h3 class="typing-text">I'm a <span></span></h3>
+                <p>
+                    Hi, I'm Kendy Hashimoto, a Computer Science student with strong interpersonal skills. I'm proactive,
+                    organized, a fast learner, and a team player.
+                    I'm looking for an internship or entry-level position in
+                    the tech field to build my career. I have hands-on experience in both front-end and back-end development
+                    using Python, JavaScript, React, Bootstrap, HTML, and CSS, through school and personal projects. I'm eager to contribute to
+                    a team and expand my knowledge.
+                </p>
+                <div class="social-icon">
+                    <a target="blank" href="https://www.linkedin.com/in/kendy-hashimoto-944439304/"><i class="fa-brands fa-linkedin"></i></a>
+                    <a target="blank" href="https://github.com/notknd"><i class="fa-brands fa-github"></i></a>
+                    <a target="blank" href="https://www.instagram.com/n0tknd/" ><i class="fa-brands fa-instagram"></i></a>
+                </div>
+                <a href="#" class="btn">Hire Me</a>
+            </div>
+        </section>
+    );
+};
+
+export default Main;
